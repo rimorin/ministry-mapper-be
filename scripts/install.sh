8bb5575610e50b1efd968342e99a7cd7d7ff2906
Kwan Rimorin John Eric <rimorin@gmail.com>
2026-05-08 22:05:08 +0800

    chore: add pre-push hook with unit and integration tests

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,5 +8,10 @@ go get ./...
 echo "🧹 Tidying up go.mod..."
 go mod tidy
 
+# Set up git hooks
+echo "🪝 Setting up git hooks..."
+cp .githooks/pre-push .git/hooks/pre-push
+chmod +x .git/hooks/pre-push
+
 # Success message
 echo "✅ Go packages installed and initialized successfully."
